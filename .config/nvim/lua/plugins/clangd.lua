da3633eb2eecf96d56a891bc0d8a993c5532b763
humbornjo <humbornjo@gmail.com>
2025-01-19 16:09:44 +0800

    fix: tpm install
    update: change gruvbox source
    
    update: change gruvbox source

--- .config/nvim/lua/plugins/clangd.lua
+++ .config/nvim/lua/plugins/clangd.lua
@@ -18,14 +18,14 @@ return {
     init = function()
       -- load clangd extensions when clangd attaches
       local augroup =
-        vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
+          vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
       vim.api.nvim_create_autocmd("LspAttach", {
         group = augroup,
         desc = "Load clangd_extensions with clangd",
         callback = function(args)
           if
-            assert(vim.lsp.get_client_by_id(args.data.client_id)).name
-            == "clangd"
+              assert(vim.lsp.get_client_by_id(args.data.client_id)).name
+              == "clangd"
           then
             require("clangd_extensions")
             -- add more `clangd` setup here as needed such as loading autocmds
