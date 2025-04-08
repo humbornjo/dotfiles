d06495ef85e8800605e353f2cd414ca429387978
humbornjo <humbornjo@gmail.com>
2025-04-08 23:26:25 +0800

    fix: conflict with zz

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -5,10 +5,10 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "z",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "Z",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "m",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "M",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = { "o" },           function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    { "<c-m>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
   },
 }
