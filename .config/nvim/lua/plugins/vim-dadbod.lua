83863533e4c79d307ac8b80c614bccafc57aa4d4
humbornjo <humbornjo@gmail.com>
2025-06-20 23:41:22 +0800

    fix(nvim): dadbod full plugin name

--- .config/nvim/lua/plugins/vim-dadbod.lua
+++ .config/nvim/lua/plugins/vim-dadbod.lua
@@ -2,7 +2,7 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-    dependencies = "vim-dadbod",
+    dependencies = { "tpope/vim-dadbod" },
     keys = {
       { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
     },
