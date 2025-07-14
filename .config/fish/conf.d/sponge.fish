846b3054c35a2dda0b9dcff05cc6d554270c38db
humbornjo <humbornjo@gmail.com>
2025-07-15 03:00:27 +0800

    fix(fish): longer history delay

--- .config/fish/conf.d/sponge.fish
+++ .config/fish/conf.d/sponge.fish
@@ -3,7 +3,7 @@ set --global sponge_version 1.1.0
 
 # Allow to repeat previous command by default
 if not set --query --universal sponge_delay
-  set --universal sponge_delay 5
+  set --universal sponge_delay 20
 end
 
 # Purge entries both after `sponge_delay` entries and on exit by default
@@ -23,7 +23,7 @@ end
 
 # Consider `0` the only successful exit code by default
 if not set --query --universal sponge_successful_exit_codes
-  set --universal sponge_successful_exit_codes 0 130
+  set --universal sponge_successful_exit_codes 0
 end
 
 # No active regex patterns by default
